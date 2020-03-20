ee65436fecaa754f049e2f3e3d15f9c823880a91
shloimimink <shloimimink@gmail.com>
2020-03-20 12:01:51 -0500

    component structure

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -6,7 +6,6 @@ class TodoList extends Component {
         return (
             <section>
                 <h2> todo list</h2>
-                <p>hello</p>
                 <Item/>
             </section>
 
