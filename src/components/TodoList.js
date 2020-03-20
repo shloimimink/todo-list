a1d248ca99859240601700c7ce90fb30e1ed6a01
shloimimink <shloimimink@gmail.com>
2020-03-20 11:05:46 -0500

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Item from "./TodoItem";
+
+class TodoList extends Component {
+    render() {
+        return (
+            <section>
+                <h2> todo list</h2>
+                <Item/>
+            </section>
+
+        );
+    }
+}
+
+export default TodoList;
